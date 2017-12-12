630f60fd93007d5df45c0dd67df34485c1c768cb
Ian Munene <munene.ian@live.com>
2017-12-12 23:28:19 +0300

    stopwatch layout complete

--- app/src/main/java/com/ianmunene/stopwatch/MainActivity.java
+++ app/src/main/java/com/ianmunene/stopwatch/MainActivity.java
@@ -2,6 +2,7 @@ package com.ianmunene.stopwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStart(View view) {
+    }
+
+    public void onClickStop(View view) {
+    }
+
+    public void onClickReset(View view) {
     }
 }
