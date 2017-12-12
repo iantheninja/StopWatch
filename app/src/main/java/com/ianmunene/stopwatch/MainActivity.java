c10a335f9c40bcefdb8f848da0ea7f6451ae298f
Ian Munene <munene.ian@live.com>
2017-12-12 23:55:03 +0300

    functionality complete. handler added to update code on UI

--- app/src/main/java/com/ianmunene/stopwatch/MainActivity.java
+++ app/src/main/java/com/ianmunene/stopwatch/MainActivity.java
@@ -1,23 +1,57 @@
 package com.ianmunene.stopwatch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        runTimer();
     }
 
     public void onClickStart(View view) {
+        running = true;
     }
 
     public void onClickStop(View view) {
+        running = false;
     }
 
     public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView timeView = findViewById(R.id.tv_time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+
     }
 }
